46fed85d1b76b76f3efca7b9a3fa4ada0fa5be8f
gonnbar <gonzalonicolasbarbosa@gmail.com>
2026-06-04 09:09:32 -0300

    fix user model & migration / add user controller,route,schema & middlewares

---
+++ routes/users.routes.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const usrCtrl = require("../controllers/user.controller");
+const validarUser = require("../middlewares/validarUser")
+const validarUserId = require("../middlewares/validarUserId")
+const router = Router();
+
+
+router.get("/", usrCtrl.obtenerUsers);
+router.get("/:id", validarUserId, usrCtrl.obtenerUser);
+router.post("/",validarUser, usrCtrl.crearUser);
+router.put("/:id", validarUserId, validarUser, usrCtrl.actualizarUser);
+router.delete("/:id",validarUserId, usrCtrl.eliminarUser);
+
+module.exports = router;
